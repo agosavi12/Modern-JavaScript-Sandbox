0913c163ee0302e127b7acc1867cea8e9b2cbc71
Amol Gosavi <agosavi12@gmail.com>
2022-04-22 10:14:50 +0530

    string-methods

---
+++ string-methods/script.js
@@ -0,0 +1,11 @@
+const email = 'amol@frontendhub.in'
+
+//const result = email.lastIndexOf('n')
+
+// const result = email.slice(0, 4)
+
+// const result = email.substr(0, 5)
+
+//const result = email.replace('a', 'j')
+
+console.log(result)
